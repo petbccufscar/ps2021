b5e7230a6c07b595e02296a42e7f5ff3123fe551
geovanne-mansano <80107769+geovanne-mansano@users.noreply.github.com>
2021-04-13 18:49:55 -0300

    Add files via upload

---
+++ Fase 2663/2663.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main () {
+ 
+ int n, k, n_vetor[1000], i;
+
+ scanf("%d", &n);
+ scanf("%d", &k);
+
+ for (i=0; i<n; i++) {
+
+     scanf("%d", &n_vetor[i]);
+
+ }
+ 
+  int armazena;
+ 
+for(int j=0; j<n; j++) {
+    for (i = j+1; i<n; i++) {
+
+        if (n_vetor[j] <  n_vetor[i]) {
+            
+            armazena = n_vetor[j];
+            n_vetor[j] = n_vetor[i];
+            n_vetor[i] = armazena;
+        }
+     
+    }
+}
+
+int classificados = k;
+i=1;
+
+while(n_vetor[k-1] == n_vetor[k-1+i]) {
+	
+	classificados++;
+	i++;
+}
+
+printf("%d\n", classificados);
+
+return 0;
+
+}
