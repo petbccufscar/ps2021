5eeeb048c81cb7b15ad1ee3c238169fad8ea1874
ThePaladini <82978181+ThePaladini@users.noreply.github.com>
2021-04-21 19:36:46 -0300

    Update 2520.c

--- Respostas2520/2520.c
+++ Respostas2520/2520.c
@@ -7,31 +7,24 @@ int main(int argc, char *argv[]) {
 	
 	
   
-  int linhas, colunas, LinhaOrigem,ColunaOrigem,LinhaDestino,ColunaDestino,dist;
-  scanf("%d %d", &linhas,&colunas);		
+  int linhas, colunas,dist,aux,linCord[2],colCord[2];
   int i,j; //Variáveis Auxiliares 
-  int **m = (int**)malloc(linhas * sizeof(int*)); //Aloca um Vetor de Ponteiros
- 
-  for (i = 0; i < linhas; i++){ //Percorre as linhas do Vetor de Ponteiros
-       m[i] = (int*) malloc(colunas * sizeof(int)); //Aloca um Vetor de Inteiros para cada posição do Vetor de Ponteiros.
-       for (j = 0; j < colunas; j++){ //Percorre o Vetor de Inteiros atual.
-            scanf("%d", &m[i][j]);
-       }
+  while (scanf("%d %d", &linhas,&colunas) != EOF){
+  		aux=0;
+  		int **m = (int**)malloc(linhas * sizeof(int*)); 
+  		for (i = 0; i < linhas; i++){ 
+        	m[i] = (int*) malloc(colunas * sizeof(int)); 
+      		for (j = 0; j < colunas; j++){
+            	scanf("%d", &m[i][j]);
+            		if(m[i][j]!=0){
+		       		linCord[aux]=i;
+		       		colCord[aux]=j;
+		       		aux++;
+					}
+       		}
+		}
+		dist=abs(linCord[0]-linCord[1]) + abs(colCord[0]-colCord[1]);
+		printf("%d\n", dist);
 	}
-	for (i = 0; i < linhas; i++){
-       for (j = 0; j < colunas; j++){ 
-	       	if(m[i][j]==1){
-	       		LinhaOrigem=i;
-	       		ColunaOrigem=j;
-			}
-			else if(m[i][j]==2){
-				LinhaDestino=i;
-				ColunaDestino=j;
-			}
-			
-       }   
-	}
-	dist=abs(LinhaDestino-LinhaOrigem) + abs(ColunaDestino-ColunaOrigem);
-	printf("O tempo para tingir será: %d segundos", dist);
 	return 0;
 }
